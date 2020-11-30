97c5aa5787a1475e5c99b6b0920a99a2c6dfa636
abhijay-pradhan <73331596+abhijay-pradhan@users.noreply.github.com>
2020-11-30 15:21:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 function stt() {
-window.open("https://firebasestorage.googleapis.com/v0/b/shiveshchat.appspot.com/o/index.html?alt=media&token=73d71921-143f-4d02-8106-0a131847b257", "_blank", "width=1000, height=800");
+window.open("https://0fvihxxcyeszhg3yy2pcaa-on.drv.tw/Abhijaypradhanchannel/practicetests/phy-001/", "_blank", "width=1000, height=800");
 }
