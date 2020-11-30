4972290a89589c41176d05b9512d0c1065a55048
abhijay-pradhan <73331596+abhijay-pradhan@users.noreply.github.com>
2020-11-30 16:11:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 function stt() {
-window.open("https://0fvihxxcyeszhg3yy2pcaa-on.drv.tw/Abhijaypradhanchannel/practicetests/phy-001/", "_blank", "width=1000, height=800");
+window.open("https://0fvihxxcyeszhg3yy2pcaa-on.drv.tw/Abhijaypradhanchannel/practicetests/bio-001/", "_blank", "width=1000, height=800");
 }
